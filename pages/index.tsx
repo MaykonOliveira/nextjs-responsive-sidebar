ab9863c523b7e9e90f046fa706bd563c2b5e5743
Maykon A. Oliveira <moliveira.anschau@gmail.com>
2022-01-30 11:40:50 -0300

    created responsive sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,109 @@
 import Head from 'next/head'
+import { MenuIcon, SearchIcon, UserIcon, ViewGridIcon } from '@heroicons/react/outline'
+import { useState } from 'react'
 
 export default function Home() {
+  const [active, setActive] = useState(false)
+
+  function toggleSidebar() {
+    setActive(!active)
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen text-text-color">
       <Head>
-        <title>Create Next App</title>
+        <title>Sidebar responsive study</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <aside className={`
+        transition-all duration-300
+        flex flex-col items-center justify-between bg-green
+        ${active ? 'w-64' : 'w-20'}
+      `}>
+        <div className="p-4 w-full space-y-6">
+          <div className="flex items-center justify-between">
+            <img className={`
+              ${active ? 'block' : 'hidden'}
+            `}
+              src="/assets/logo.svg" />
+            <button className="p-3 rounded-xl hover:bg-light-green" onClick={toggleSidebar}>
+              <MenuIcon className="h-6 w-6" />
+            </button>
+          </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+          <form className="flex items-center justify-center p-3 bg-light-green rounded-xl">
+            <SearchIcon className={`h-6 w-6 ${ active ? 'pointer-events-none' : 'cursor-pointer'}`} onClick={toggleSidebar} />
+            <input className={`
+              w-full outline-none border-none ml-[0.5rem] bg-light-green
+              ${active ? 'block' : 'hidden'}
+            `}
+              placeholder="Buscar"></input>
+          </form>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+          <div className="space-y-3">
+            <button className="flex w-full p-3 rounded-xl hover:bg-light-green">
+              <img className="h-6 w-6" src="/assets/grid.svg" />
+              <span className={`
+                ml-3
+                ${active ? 'block' : 'hidden'}
+              `}>Dashboard</span>
+            </button>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+            <button className="flex w-full p-3 rounded-xl hover:bg-light-green">
+              <img className="h-6 w-6" src="/assets/pet.svg" />
+              <span className={`
+                ml-3
+                ${active ? 'block' : 'hidden'}
+              `}>Pets</span>
+            </button>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+            <button className="flex w-full p-3 rounded-xl hover:bg-light-green">
+              <img className="h-6 w-6" src="/assets/user.svg" />
+              <span className={`
+                ml-3
+                ${active ? 'block' : 'hidden'}
+              `}>Clientes</span>
+            </button>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <button className="flex w-full p-3 rounded-xl hover:bg-light-green">
+              <img className="h-6 w-6" src="/assets/user-md.svg" />
+              <span className={`
+                ml-3
+                ${active ? 'block' : 'hidden'}
+              `}>Vets</span>
+            </button>
+
+            <button className="flex w-full p-3 rounded-xl hover:bg-light-green">
+              <img className="h-6 w-6" src="/assets/settings.svg" />
+              <span className={`
+                ml-3
+                ${active ? 'block' : 'hidden'}
+              `}>Ajustes</span>
+            </button>
+          </div>
         </div>
-      </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
+        <div className="flex items-center justify-between w-full h-16 px-3 py-2 bg-light-green">
+          <div className={`
+            w-full space-x-3
+            ${active ? 'flex' : 'hidden'}
+          `}>
+            <img className="h-11 w-11 rounded-xl" src="/assets/avatar.svg" />
+            <div className="flex flex-col items-left justify-center">
+              <span className="text-sm">Eleanor Pena</span>
+              <span className="text-xs">Veterinária</span>
+            </div>
+          </div>
+
+          <button className="p-3 rounded-xl hover:bg-green">
+            <img className="h-6 w-6" src="/assets/log-out.svg" />
+          </button>
+        </div>
+      </aside>
+      <main className="flex flex-grow p-7 bg-body-bg-color">
+        <h2 className="font-semibold	">Dashboard</h2>
+      </main>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      'body-bg-color': '#dce4e3',
+      'green': '#18c29c',
+      'light-green': '#8ed7c6',
+      'light-grey': '#dce4e3',
+      'text-color': '#084236'
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
   },
   plugins: [],
 }
